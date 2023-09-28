14b6b82b70fa2f2adde91940a92d30da01fbcc28
Ranym <ranimmehri@gmail.com>
2023-09-28 15:45:52 +0200

    pushing my first code to github

---
+++ game-store-backend/src/main/java/com/game_store/gamestorebackend/GameStoreBackendApplication.java
@@ -0,0 +1,13 @@
+package com.game_store.gamestorebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameStoreBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GameStoreBackendApplication.class, args);
+	}
+
+}
---
+++ game-store-backend/src/test/java/com/game_store/gamestorebackend/GameStoreBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.game_store.gamestorebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameStoreBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
