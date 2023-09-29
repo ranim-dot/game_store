10309440386e3a0bbcc9228cc91ceb3f6d22c6f3
Ranym <ranimmehri@gmail.com>
2023-09-29 09:55:45 +0200

    Adding react files

---
+++ game-store-frontend/src/App.js
@@ -0,0 +1,17 @@
+
+import './App.css';
+import Button from "@mui/material/Button"
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Button variant='contained'>
+          Hello
+        </Button>
+      </header>
+    </div>
+  );
+}
+
+export default App;
